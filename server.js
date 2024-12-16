02828230e1cad834d29e8b2b7c73108c1d1ae7a5
BelgyBoi <tibo.kooreman@gmail.com>
2024-12-16 15:27:23 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+app.use('/videos', express.static('videos'));
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('playVideo', (videoId) => {
+    io.emit('playVideo', videoId);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
